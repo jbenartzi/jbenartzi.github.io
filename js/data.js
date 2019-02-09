c48704a514509b54bcb6d15dced28c05a7d371fe
jbenartzi <jonathan.benartzi@gmail.com>
2019-02-09 20:13:16 +0000

    2019.02.09

---
+++ js/data.js
@@ -0,0 +1,34 @@
+var data = [
+	{
+		"name": "Intradisciplinary Lecture Series",
+		"gcal": "8npmcuo5sfjjp4id7bvq84hof4@group.calendar.google.com"
+	},
+	{
+		"name": "Analysis & PDE",
+		"gcal": "cardiff.analysis@gmail.com"
+	},
+	{
+		"name": "Informal Analysis & PDE Seminar",
+		"gcal": "guleaka48tbjspp5v1d8e4ubns@group.calendar.google.com"
+	},
+	{
+		"name": "Geometry, Algebra, Mathematical Physics and Topology Seminar",
+		"gcal": "gsqvsk0b6bin4ggivkg5mkdfmk@group.calendar.google.com"
+	},
+	{
+		"name": "Mathematics Colloquium",
+		"gcal": "frj8be6mpo43m407rucq0ojgj8@group.calendar.google.com"
+	},
+	{
+		"name": "Applied and Computational Mathematics",
+		"gcal": "e3ju97o769nj95ph7dpmvbi61o@group.calendar.google.com"
+	},
+	{
+		"name": "Workshops",
+		"gcal": "ssr6d03mfb8vr5qglq1h4qnrgg@group.calendar.google.com"
+	},
+	{
+		"name": "OR/Statistics Seminar",
+		"gcal": "0l5tqnll32uj9q16vb3pkkvmj8@group.calendar.google.comm"
+	}
+]
